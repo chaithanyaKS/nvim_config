3166492586abcb6974d1ad028395faa91ee2a965
ChaithanyaKS <ChaithanyaKS@radware.com>
2025-01-01 13:11:47 +0000

    removed tabline and fixed which key warning

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -32,8 +32,6 @@ return { -- Collection of various small independent plugins/modules
       return '%2l:%-2v'
     end
 
-    require('mini.tabline').setup {}
-
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,

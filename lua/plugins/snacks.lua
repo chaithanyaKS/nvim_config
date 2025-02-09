96e4729a0b2c071f18e2a0741daec76559f4c630
ChaithanyaKS <ChaithanyaKS@radware.com>
2025-02-09 06:56:15 +0000

    indent line

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -11,7 +11,7 @@ return {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
     -- explorer = { enabled = true },
-    -- indent = { enabled = true },
+    indent = { enabled = true, animate = { enabled = false } },
     input = { enabled = true },
     -- picker = { enabled = true },
     -- notifier = { enabled = true },

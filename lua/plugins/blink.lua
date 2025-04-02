de1f3d711b9b6c6538e515514d9ff9a08f83da4f
chaithanyaKS <chaithanya2934@gmail.com>
2025-04-02 21:01:56 +0530

    added preselect config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -34,7 +34,7 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = true }, list = { selection = { auto_insert = false } } },
+    completion = { documentation = { auto_show = true }, list = { selection = { auto_insert = false, preselect = true } } },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`

ddf5036ef9872f12853a5ed949ff90a9f9e27a47
chaithanya <chaithanya2934@gmail.com>
2025-11-27 22:37:39 +0530

    Updated code companion

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -4,7 +4,7 @@ return {
     'nvim-lua/plenary.nvim',
     'ravitemer/mcphub.nvim',
     { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'markdown', 'codecompanion' } },
-    'franco-ruggeri/codecompanion-spinner.nvim',
+    'j-hui/fidget.nvim',
   },
   opts = {
     strategies = {
@@ -74,4 +74,36 @@ return {
       log_level = 'DEBUG',
     },
   },
+
+  config = function()
+    require('codecompanion').setup {}
+    local progress = require 'fidget.progress'
+    local handles = {}
+    local group = vim.api.nvim_create_augroup('CodeCompanionFidget', {})
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'CodeCompanionRequestStarted',
+      group = group,
+      callback = function(e)
+        handles[e.data.id] = progress.handle.create {
+          title = 'CodeCompanion',
+          message = 'Thinking...',
+          lsp_client = { name = e.data.adapter.formatted_name },
+        }
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'CodeCompanionRequestFinished',
+      group = group,
+      callback = function(e)
+        local h = handles[e.data.id]
+        if h then
+          h.message = e.data.status == 'success' and 'Done' or 'Finished'
+          h:finish()
+          handles[e.data.id] = nil
+        end
+      end,
+    })
+  end,
 }
